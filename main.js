b5af91e37a987cbb6ff8c384a29c740c7c0d7b98
sebastian <sebasr1268@gmail.com>
2023-09-21 13:25:52 -0500

    second commit

--- main.js
+++ main.js
@@ -1,25 +1,61 @@
+let num1=0
+let signo;
+
+
 let botones=document.querySelectorAll(".btn")
 let mostrar =document.querySelector(".mostrar")
+let signos = document.querySelectorAll(".signo")
+
 for(let i=0; i < botones.length; i++){
     botones[i].addEventListener("click",()=>{
-        console.log(botones[i])
         let resul =botones[i].value
         mostrar.textContent+=resul
     })
 }
 
-function resultado(num){
-    let numero=0
-    numero+=num
-    
+function resultado(){
+    let num2=parseInt( mostrar.textContent)
+    let resultado;
+    if(signo == "+"){
+        resultado=num2+num1
+    }
+    else if(signo == "-"){
+        resultado=num1-num2
+    }
+    else if(signo == "*"){
+        resultado=num1*num2
+    }
+    else if(signo == "/"){
+        resultado=num1/num2
+    }
+    mostrar.textContent=resultado
+
 
 }
+signos.forEach(element => {
+    element.addEventListener("click",()=>{
+        num1 =parseInt(mostrar.textContent)
+        mostrar.textContent=""
+
+        if (element.value == "+"){
+            signo="+"
+        }
+        
+        else if (element.value == "-"){
+            signo="-"
+        }
+
+        else if (element.value == "X"){
+            signo="*"
+        }
+
+        else if (element.value == "%"){
+            signo="/"
+        }
+        
+    })
+});
 
 function ac (){
     mostrar.textContent=""
-}
-function sumar(){
-    let num1 =parseInt(mostrar.textContent)
-    mostrar.textContent=""
-    resultado(num1)
 }
